3c12c3fa7a6c8f656ac6c7d7b3b982e1ec0fc1a9
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-02-20 19:24:52 +0100

    Parallelize extract_mentions function #3

--- src/utils/mention_util.py
+++ src/utils/mention_util.py
@@ -44,31 +44,32 @@ def find_search_term(transcript, search_term):
   return extracted_strings
 
 
-def extract_mentions(df, search_terms):
+def extract_mentions(row, search_terms):
     # Create an empty list to store the rows of the new DataFrame
   rows = []
 
-  # Iterate over the rows of the original DataFrame
-  for _, row in tqdm(df.iterrows(), total=df.shape[0]):
-    medium = row['medium']
-    id = row['id']
-    title = row['title']
-    minute = row['minute']
-    transcript = row['transcript']
-    date = row['date']
+# Iterate over the rows of the original DataFrame
+  medium = row['medium']
+  id = row['id']
+  title = row['title']
+  minute = row['minute']
+  transcript = row['transcript']
+  date = row['date']
 
 
-    # For each search term, extract the relevant strings and add a row to the new DataFrame for each occurrence
-    for term in search_terms.keys():
-      extracted_strings = find_search_term(transcript, term)
-      if extracted_strings:
-        for extracted_string in extracted_strings:
-          rows.append({'medium': medium,
-                        'id': id, 
-                        'title': title, 
-                        'minute': minute, 
-                        'date': date, 
-                        'search_term': term, 
-                        'extracted_string': extracted_string})
+  # For each search term, extract the relevant strings and add a row to the new DataFrame for each occurrence
+  for term in search_terms.keys():
+    extracted_strings = find_search_term(transcript, term)
+    if extracted_strings:
+      for extracted_string in extracted_strings:
+        rows.append({'medium': medium,
+                      'id': id, 
+                      'title': title, 
+                      'minute': minute, 
+                      'date': date, 
+                      'search_term': term, 
+                      'extracted_string': extracted_string})
 
+  if not rows:
+    return
   return rows
