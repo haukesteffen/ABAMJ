376b80eee9827a11ec9ece0a81a6177e95c1c7be
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-02-22 19:53:10 +0100

    Add transformers to setup.sh #11

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ pip install pandarallel
 pip install tqdm
 pip install youtube-transcript-api
 pip install youtube-search-python
+pip install transformers
 
 OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES python3 src/scraper.py
