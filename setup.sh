1c8fe2fb9c26d54b584c63f58c4123026c5534c7
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-02-27 18:43:24 +0100

    Add matplotlib and seaborn to setup.sh #11

--- setup.sh
+++ setup.sh
@@ -6,5 +6,7 @@ pip install youtube-transcript-api
 pip install youtube-search-python
 pip install transformers
 conda install pytorch -c pytorch
+pip install matplotlib
+pip install seaborn
 
 OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES python3 src/scraper.py
