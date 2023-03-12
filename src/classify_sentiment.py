4df91802600e314477b8fd3ac069b6dbdc9669f2
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-03-12 17:03:17 +0100

    Add verbosity to classify_sentiment.py #16

--- src/classify_sentiment.py
+++ src/classify_sentiment.py
@@ -5,12 +5,17 @@ import pandas as pd
 
 
 def classify_sentiment():
+  # Import mention data
   party_df = pd.read_pickle('data/party_mentions.pkl')
   politician_df = pd.read_pickle('data/politician_mentions.pkl')
 
+  # Classify sentiment of party mentions and save pickled dataframe
+  print('\n\nClassifying sentiment of party mentions...')
   sentiment_classified_party_df = extract_sentiment_df(party_df)
   sentiment_classified_party_df.to_pickle('data/party_sentiment_classifications.pkl')
 
+  # Classify sentiment of politician mentions and save pickled dataframe
+  print('\n\nClassifying sentiment of politician mentions...')
   sentiment_classified_politician_df = extract_sentiment_df(politician_df)
   sentiment_classified_politician_df.to_pickle('data/politician_sentiment_classifications.pkl')
 
